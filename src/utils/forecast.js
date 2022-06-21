a1c56963b6083711c2d393aca6e6d620855964cd
Reiv <reivin.bigalbal@accenture.com>
2022-06-21 15:17:10 +0800

    Some changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,6 +9,7 @@ const forecast = (latitude, longitude, callback) =>{
         if(error){
             callback('Unable to connect to weather service!', undefined)
         }else if(body.error){
+            console.log(body)
             callback(body.error.info , undefined)
         }else{
             callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
