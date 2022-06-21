8d754d9bee88e6ba3449c1959569296683afd639
Reiv <reivin.bigalbal@accenture.com>
2022-06-21 15:43:28 +0800

    change message at forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,10 +9,9 @@ const forecast = (latitude, longitude, callback) =>{
         if(error){
             callback('Unable to connect to weather service!', undefined)
         }else if(body.error){
-            console.log(body)
             callback(body.error.info , undefined)
         }else{
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%`)
         }
     })
 }
